f7dbbff65cbbe212970272fe1530b12d71c0553e
Miggachu <miguel.silverhagen@edu.prakticum.fi>
2023-02-27 13:08:57 +0200

    testmaterial

---
+++ ImpCsv.ps1
@@ -0,0 +1,87 @@
+﻿Remove-Item "C:\temp\Porvoo.db"
+
+$csvfilepath = "C:\temp\Borgo.csv"
+
+$imp = Import-Csv  $csvfilepath -Encoding UTF7 -Delimiter ';' 
+
+$Databas = "C:\temp\Porvoo.db"
+
+
+$Query = "CREATE TABLE Porvoo (id INTEGER PRIMARY KEY, 
+
+kustannus_id INTEGER, 
+kustannus_name TEXT,
+tosite_number INTEGER,
+tositename TEXT,
+euro_brutto REAL,
+rondo_id INTEGER);"
+ 
+
+invoke-SqliteQuery -Query $Query -DataSource $Databas
+$Counter = 0
+
+ $imp | ForEach-Object {
+
+ $Counter = $Counter + 1
+ $Counter
+                 #$Databas = "C:\temp\Porvoo.db"
+                 $kustannus_id = $_.Kustannuspaikka
+                 $kustannus_name = $_.'Kustannuspaikan nimi'.replace("'",'"')
+                 $tosite_number = $_.Tositenumero
+                 $tositename = $_.Selite.replace("'",'"')
+                 $euro_brutto = $_.'EUR, brutto'.replace(" ","").replace(",",".")
+                 $rondo_id = $_.'Rondo ID'
+
+                 $Query = "INSERT INTO Porvoo (kustannus_id,
+                                               kustannus_name,
+                                               tosite_number,
+                                               tositename,
+                                               euro_brutto,
+                                               rondo_id
+                                               )
+                                                 VALUES
+                                                 ( '$kustannus_id',
+                                                   '$kustannus_name',
+                                                   '$tosite_number',
+                                                   '$tositename',
+                                                   '$euro_brutto',
+                                                   '$rondo_id');"
+
+                   Invoke-SqliteQuery -Query $Query -DataSource $Databas
+}
+
+$Lista = Invoke-SqliteQuery -Query "Select * from Porvoo;" -DataSource $Databas
+
+
+$Query = "Select 
+           
+           kustannus_id,
+           kustannus_name,
+           Sum(euro_brutto)
+           FROM
+           Porvoo
+           GROUP BY
+           Kustannus_id
+           ORDER BY SUM(euro_brutto) DESC
+           Limit 5;"
+           Invoke-SqliteQuery -Query $Query -DataSource $Databas
+           
+           
+           
+           
+           
+           
+           
+                                               
+
+
+
+
+
+
+
+
+
+
+
+
