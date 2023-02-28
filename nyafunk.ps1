6b8cc9ef8abd7628647808543f223989ff80f8ce
Miggachu <miguel.silverhagen@edu.prakticum.fi>
2023-02-28 11:33:38 +0200

    testmaterial

---
+++ nyafunk.ps1
@@ -0,0 +1,22 @@
+﻿Function RunOn-Windows
+{
+Write-Host 'The Script is Running on a Windows Machine'
+}
+
+Function RunOn-Linux
+{
+Write-Host 'The Script is Running on a Linux Machine'
+}
+
+Function RunOn-Mac
+{
+Write-Host 'The Script is Running on a Mac'
+}
+
+If ($IsWindows) {RunOn-Windows}
+If ($IsLinux) {RunOn-Linux}
+If ($IsMacOS) {RunOn-Mac}
+
+
+
+[System.IO.Path]::GetTempPath()+"\Porvoo.db"
