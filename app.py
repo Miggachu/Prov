598a63aa179a738f74bb768316b9dfd6363db5a5
Miggachu <117444555+Miggachu@users.noreply.github.com>
2023-03-07 12:01:30 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template
+import sqlite3
+
+con = sqlite3.connect("Porvoo.db")
+
+cur = con.cursor()
+
+cur.execute('SELECT * FROM top5')
+
+  
+data = cur.fetchall()
+  
+con.close()
+
+#con = sqlite3.connect("Porvoo.db")
+#
+#cur = con.cursor()
+#
+#cur.execute('SELECT * FROM hexcol')
+#
+#  
+#data = cur.fetchall()
+#  
+#con.close()
+
+      
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  dataasd = [
+          ("01-01-2020", 1597),
+          ("02-01-2020", 1456),
+          ("03-01-2020", 1908),
+          ("04-01-2020", 896),
+          ("05-01-2020", 755),
+  ]
+  
+  
+  color = ["#059bf", 
+           "#ffc234", 
+           "#22cfcf", 
+           "#ff4069", 
+           "#ff9020"]
+          
+          
+  
+  labels =[]
+  values =[]
+  color =[]
+  for row in data:
+    labels.append(row[1])
+  for row in data:
+    values.append(row[2])
+  for row in data:
+    color.append("#"+row[0])
+
+  #För att debugga jinjan
+  #labels = [row[0] for row in data]
+  #values = [row[1] for row in data]
+  
+  #return data
+  return render_template("graph.html", labels=labels, values=values, color=color) 
+
+#if__name__== "__main__":app.run(port=8000, debug=True)
